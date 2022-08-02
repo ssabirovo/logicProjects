7dfe8c9cf35b2c77fc71cdfd81534ce40bd52841
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-08-02 09:16:12 +0500

    second commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import Calculator from "./projects/calculator/calculator";
+import Calculator from "./components/calculator";
 
 
 class App extends Component {
--- src/projects/weather-app/weather.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class Weather extends Component {
-  render() {
-    return <h1>Weather Component</h1>;
-  }
-}
-
-export default Weather;
