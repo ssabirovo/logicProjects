e255b1a3eb44c0d94916b934bb30b314676c0752
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-08-02 09:07:03 +0500

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import Calculator from "./projects/calculator/calculator";
+
+
+class App extends Component {
+  render() {
+    return <Calculator />;
+  }
+}
+
+export default App;
